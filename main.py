81c71d1e34d095d836363515fb0fb1d513fc2d0e
Sergey Fomin <moocher@moocher.ru>
2020-10-20 20:10:19 +0300

    Projections added

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 import sys
-from math import ceil, sin, pi, atanh, atan, exp, asin, log, tan
+from math import ceil
 from pathlib import Path
 
 from PyQt5 import QtGui
-from PyQt5.QtCore import QPoint, Qt, QPointF
+from PyQt5.QtCore import QPoint, Qt
 from PyQt5.QtGui import QPainter, QColor, QFont, QBrush
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
 
@@ -32,7 +32,7 @@ class MainWindow(QMainWindow):
 
         screen = QDesktopWidget().screenGeometry()
         self.setGeometry(screen.width() // 4, screen.height() // 4, screen.width() // 2, screen.height() // 2)
-        self.setWindowTitle('Draw text')
+        self.setWindowTitle('GIS')
         self.calc_geo()
         self.show()
 
@@ -148,57 +148,6 @@ class MainWindow(QMainWindow):
 
     def closeEvent(self, event):
         SETTINGS.write_text(json.dumps({'zoom': self.zoom_level, 'lat': self.central_point_geo.lat, 'lon': self.central_point_geo.lon}))
-
-
-class Projection:
-    def to_pixel(self, coordinates: LonLat):
-        pass
-
-
-class MercatorProjection(Projection):
-    VRadiusA = 6378137
-    VRadiusB = 6356752
-    MerkElipsK = 0.000000001
-    fexct = (VRadiusA * VRadiusA - VRadiusB * VRadiusB) ** (1 / 2) / VRadiusA
-
-    def to_pixel(self, coordinates: LonLat):
-        x = (0.5 + coordinates.lat / 360)
-        z = sin(coordinates.lon * pi / 180)
-        c = (1 / (2 * pi))
-        y = (0.5 - c * (atanh(z) - self.fexct * atanh(self.fexct * z)))
-
-        return QPointF(x, y)
-
-    def to_geo(self, point: QPointF) -> LonLat:
-        x = (point.x() - 0.5) * 360
-
-        if point.y() > 0.5:
-            yy = (point.y() - 0.5)
-        else:
-            yy = (0.5 - point.y())
-
-        yy = yy * (2 * pi)
-        zu = 2 * atan(exp(yy)) - pi / 2
-        e_y = exp(2 * yy)
-        y = zu * (180 / pi)
-        while True:
-            zum1 = zu
-            v_sin = sin(zum1)
-            zu = asin(1 - (1 + v_sin) * pow((1 - self.fexct * v_sin) / (1 + self.fexct * v_sin), self.fexct) / e_y)
-            if abs(zum1 - zu) < self.MerkElipsK:
-                break
-
-        if point.y() > 0.5:
-            y = -zu * 180 / pi
-        else:
-            y = zu * 180 / pi
-        return LonLat(x, y)
-
-    def y2lat(self, a):
-        return 180.0 / pi * (2.0 * atan(exp(a * pi / 180.0)) - pi / 2.0)
-
-    def lat2y(self, a):
-        return 180.0 / pi * log(tan(pi / 4.0 + a * (pi / 180.0) / 2.0))
 
 
 if __name__ == '__main__':
