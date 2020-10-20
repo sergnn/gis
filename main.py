07e838352ad2edbc67e0007c24ee2327a8807f41
Sergey Fomin <moocher@moocher.ru>
2020-10-20 19:56:19 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,209 @@
+import json
+import sys
+from math import ceil, sin, pi, atanh, atan, exp, asin, log, tan
+from pathlib import Path
+
+from PyQt5 import QtGui
+from PyQt5.QtCore import QPoint, Qt, QPointF
+from PyQt5.QtGui import QPainter, QColor, QFont, QBrush
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
+
+from utils.tiles import LonLat, deg_per_tile, TILE_SIZE, tile_geo, YandexSatellite
+
+SETTINGS = Path('settings.json')
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.drag_start_point = QPoint(0, 0)
+        self.left_mouse_button_pressed = False
+
+        settings = json.loads(SETTINGS.read_text())
+
+        # Init zoom
+        self.zoom_level = settings['zoom']
+
+        # Central point Lat (-90 - 90), Lon (0 - 180)
+        self.central_point_geo = LonLat(settings['lon'],
+                                        settings['lat'])
+
+        screen = QDesktopWidget().screenGeometry()
+        self.setGeometry(screen.width() // 4, screen.height() // 4, screen.width() // 2, screen.height() // 2)
+        self.setWindowTitle('Draw text')
+        self.calc_geo()
+        self.show()
+
+    def calc_geo(self):
+        self.deg_per_tile = deg_per_tile(self.zoom_level)
+
+        self.deg_per_pixel = LonLat(self.deg_per_tile.lon / TILE_SIZE,
+                                    self.deg_per_tile.lat / TILE_SIZE)
+
+        self.central_point_tile = QPoint(int((self.central_point_geo.lon + LonLat.TOTAL_LON / 2) / self.deg_per_tile.lon),
+                                         int((LonLat.TOTAL_LAT / 2 - self.central_point_geo.lat) / self.deg_per_tile.lat))
+
+        self.window_width = self.geometry().width()
+        self.window_height = self.geometry().height()
+
+        self.tiles_count = QPoint(ceil(self.window_width / TILE_SIZE),
+                                  ceil(self.window_height / TILE_SIZE))
+
+        self.central_point = QPoint(self.window_width // 2,
+                                    self.window_height // 2)
+
+        self.central_point_tile_geo = tile_geo(self.central_point_tile.x(), self.central_point_tile.y(), self.zoom_level)
+        print(self.central_point_tile_geo.lat, self.central_point_tile_geo.lon)
+        self.tile_shift = QPoint(-int((self.central_point_geo.lon - self.central_point_tile_geo.lon) / self.deg_per_pixel.lon),
+                                 int((self.central_point_geo.lat - self.central_point_tile_geo.lat) / self.deg_per_pixel.lat))
+        print(self.central_point_tile)
+        print(self.tile_shift)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+
+        qp.setFont(QFont('Calibri', 10))
+        self.draw_text(event, qp)
+        qp.end()
+
+    def wheelEvent(self, event):
+        wheel_up = event.angleDelta().y() > 0
+
+        self.zoom_level += 1 if wheel_up else - 1
+        if self.zoom_level < 1:
+            self.zoom_level = 1
+
+        self.calc_geo()
+        self.repaint()
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.left_mouse_button_pressed = a0.button() == Qt.LeftButton
+        self.drag_start_point = QPoint(a0.x(), a0.y())
+        self.drag_start_central_point_geo = self.central_point_geo
+
+    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
+        if a0.button() == Qt.LeftButton:
+            self.left_mouse_button_pressed = False
+
+    def mouseMoveEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.central_point_geo = LonLat(self.drag_start_central_point_geo.lon - (a0.x() - self.drag_start_point.x()) * self.deg_per_pixel.lon,
+                                        self.drag_start_central_point_geo.lat + (a0.y() - self.drag_start_point.y()) * self.deg_per_pixel.lat)
+        self.calc_geo()
+        self.repaint()
+
+    def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
+        self.calc_geo()
+        self.repaint()
+
+    def draw_tile(self, x, y, z, qp):
+        tile = YandexSatellite().get_tile(self.central_point_tile.x() + x,
+                                          self.central_point_tile.y() + y,
+                                          z)
+
+        tile_start = QPoint(x * TILE_SIZE + self.tile_shift.x() + self.central_point.x(),
+                            y * TILE_SIZE + self.tile_shift.y() + self.central_point.y())
+        qp.drawPixmap(tile_start,
+                      tile)
+        text = f'{self.central_point_tile.x() + x} {self.central_point_tile.y() + y}'
+
+        qp.drawLine(tile_start, tile_start + QPoint(TILE_SIZE, 0))
+        qp.drawLine(tile_start, tile_start + QPoint(0, TILE_SIZE))
+
+        qp.setPen(QColor(255, 0, 0))
+        qp.drawText(tile_start + QPoint(5, 15), text)
+
+    def spiral(self, width, height, z, qp, function):
+        x = y = 0
+        dx = 0
+        dy = -1
+        for _ in range(max(width, height) ** 2):
+            if (-width / 2 < x <= width / 2) and (-height / 2 < y <= height / 2):
+                function(x, y, z, qp)
+            if x == y or (x < 0 and x == -y) or (x > 0 and x == 1 - y):
+                dx, dy = -dy, dx
+            x, y = x + dx, y + dy
+
+    def draw_text(self, event, qp):
+        self.spiral(self.tiles_count.x() + 2, self.tiles_count.y() + 2, self.zoom_level, qp, self.draw_tile)
+
+        qp.setPen(QColor(255, 255, 255))
+
+        # Draw aim
+        qp.drawLine(self.central_point.x(), self.central_point.y() - 10, self.central_point.x(), self.central_point.y() - 3)
+        qp.drawLine(self.central_point.x(), self.central_point.y() + 10, self.central_point.x(), self.central_point.y() + 3)
+        qp.drawLine(self.central_point.x() - 10, self.central_point.y(), self.central_point.x() - 3, self.central_point.y())
+        qp.drawLine(self.central_point.x() + 10, self.central_point.y(), self.central_point.x() + 3, self.central_point.y())
+
+        # Draw bottom panel
+        brush = QBrush(QColor(128, 128, 128, 200))
+        qp.fillRect(0, self.window_height - 15, self.window_width, 15, brush)
+        text = f'Zoom: z{self.zoom_level} ' \
+               f'Geo: {self.central_point_geo.lat:.6f}°, {self.central_point_geo.lon:.6f}° ' \
+               f'Central Tile: {self.central_point_tile.x()}, {self.central_point_tile.y()}'
+
+        qp.drawText(3, self.window_height - 3, text)
+
+    def closeEvent(self, event):
+        SETTINGS.write_text(json.dumps({'zoom': self.zoom_level, 'lat': self.central_point_geo.lat, 'lon': self.central_point_geo.lon}))
+
+
+class Projection:
+    def to_pixel(self, coordinates: LonLat):
+        pass
+
+
+class MercatorProjection(Projection):
+    VRadiusA = 6378137
+    VRadiusB = 6356752
+    MerkElipsK = 0.000000001
+    fexct = (VRadiusA * VRadiusA - VRadiusB * VRadiusB) ** (1 / 2) / VRadiusA
+
+    def to_pixel(self, coordinates: LonLat):
+        x = (0.5 + coordinates.lat / 360)
+        z = sin(coordinates.lon * pi / 180)
+        c = (1 / (2 * pi))
+        y = (0.5 - c * (atanh(z) - self.fexct * atanh(self.fexct * z)))
+
+        return QPointF(x, y)
+
+    def to_geo(self, point: QPointF) -> LonLat:
+        x = (point.x() - 0.5) * 360
+
+        if point.y() > 0.5:
+            yy = (point.y() - 0.5)
+        else:
+            yy = (0.5 - point.y())
+
+        yy = yy * (2 * pi)
+        zu = 2 * atan(exp(yy)) - pi / 2
+        e_y = exp(2 * yy)
+        y = zu * (180 / pi)
+        while True:
+            zum1 = zu
+            v_sin = sin(zum1)
+            zu = asin(1 - (1 + v_sin) * pow((1 - self.fexct * v_sin) / (1 + self.fexct * v_sin), self.fexct) / e_y)
+            if abs(zum1 - zu) < self.MerkElipsK:
+                break
+
+        if point.y() > 0.5:
+            y = -zu * 180 / pi
+        else:
+            y = zu * 180 / pi
+        return LonLat(x, y)
+
+    def y2lat(self, a):
+        return 180.0 / pi * (2.0 * atan(exp(a * pi / 180.0)) - pi / 2.0)
+
+    def lat2y(self, a):
+        return 180.0 / pi * log(tan(pi / 4.0 + a * (pi / 180.0) / 2.0))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    ex = MainWindow()
+
+    sys.exit(app.exec_())
---
+++ utils/tiles.py
@@ -0,0 +1,118 @@
+from collections import OrderedDict
+from pathlib import Path
+from random import randint
+
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPixmap
+from requests import get
+
+TILE_SIZE = 256
+
+
+def total_tiles_on_zoom(zoom):
+    return QPoint(2 ** (zoom - 1),
+                  2 ** (zoom - 1))
+
+
+def deg_per_tile(zoom):
+    return LonLat(LonLat.TOTAL_LON / total_tiles_on_zoom(zoom).x(),
+                  LonLat.TOTAL_LAT / total_tiles_on_zoom(zoom).y())
+
+
+def tile_geo(tile_x, tile_y, tile_z):
+    dpt = deg_per_tile(tile_z)
+    return LonLat(tile_x * dpt.lon - LonLat.TOTAL_LON / 2,
+                  -tile_y * dpt.lat + LonLat.TOTAL_LAT / 2)
+
+
+def download(url, destination):
+    destination.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(destination, 'wb') as handle:
+        response = get(url, stream=True)
+
+        if not response.ok:
+            print(url, response)
+            return False
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handle.write(block)
+
+
+class LimitedSizeDict(OrderedDict):
+    def __init__(self, *args, **kwds):
+        self.size_limit = kwds.pop("size_limit", None)
+        OrderedDict.__init__(self, *args, **kwds)
+        self._check_size_limit()
+
+    def __setitem__(self, key, value):
+        OrderedDict.__setitem__(self, key, value)
+        self._check_size_limit()
+
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+            while len(self) > self.size_limit:
+                self.popitem(last=False)
+
+
+TILE_CACHE = LimitedSizeDict(size_limit=200)
+
+
+class LonLat:
+    TOTAL_LAT = 180
+    TOTAL_LON = 360
+
+    def __init__(self, lon: float, lat: float):
+        self.lat = lat  # Latitude -90 — 90 (up-down from equator)
+        self.lon = lon  # Longitude -180 — 180 (left-right)
+
+    def __str__(self):
+        return f'Lon: {self.lon}, Lat: {self.lat}'
+
+    def __eq__(self, other):
+        return self.lat == other.lat and self.lon == other.lon
+
+
+class Map:
+    def __init__(self):
+        self.map_id = 'map'
+
+    def local_tile(self, x: int, y: int, z: int) -> Path:
+        pass
+
+    def get_tile_url(self, x: int, y: int, z: int) -> str:
+        pass
+
+    def get_tile(self, x: int, y: int, z: int) -> QPixmap:
+        if x < 0 or y < 0 or z < 0:
+            return QPixmap()
+
+        tile_id = (self.map_id, x, y, z)
+        cached_tile = TILE_CACHE.get(tile_id)
+        if cached_tile:
+            return cached_tile
+
+        local_tile = self.local_tile(x, y, z)
+
+        if not local_tile.exists():
+            download(self.get_tile_url(x, y, z), local_tile)
+
+        loaded_tile = QPixmap(str(local_tile))
+        TILE_CACHE[tile_id] = loaded_tile
+
+        return loaded_tile
+
+
+class YandexSatellite(Map):
+    def __init__(self):
+        super().__init__()
+        self.map_id = 'yasat'
+
+    def local_tile(self, x, y, z):
+        return (CACHE / self.map_id / f'z{z}' / '0' / f'x{x}' / '0' / f'y{y}').with_suffix('.jpg')
+
+    def get_tile_url(self, x, y, z):
+        return f'https://sat0{randint(1, 4)}.maps.yandex.net/tiles?l=sat&scale=1&lang=ru_RU&x={x}&y={y}&z={z - 1}'
